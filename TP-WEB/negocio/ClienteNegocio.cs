362505319b0ee658b5b94548a74f69eef261bb91
FloresSebastian23 <florescsebastian.23@gmail.com>
2025-10-01 20:43:43 -0300

    Formulario usuario
    Formulario y código para la ventana de datos de cliente, queda probar para ver si falta algún detalle

--- TP-WEB/negocio/ClienteNegocio.cs
+++ TP-WEB/negocio/ClienteNegocio.cs
@@ -35,5 +35,46 @@ namespace negocio
                 datos.cerrarConexion();
             }
         }
+
+        // Metodo para obtener por dni
+        public bool ObtenerPorDNI(Cliente cliente)
+        {
+            bool encontrado = false;
+
+            try
+            {
+                AccesoDatos datos = new AccesoDatos();
+                {
+                    // Consulta para buscar al cliente por DNI
+                    datos.setearConsulta("SELECT ID, Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP " +
+                                      "FROM Clientes WHERE Documento = @Documento");
+                    datos.setearParametro("@Documento", cliente.Documento);
+
+                    datos.ejecutarLectura();
+
+                    if (datos.Lector.Read()) // si encontró un registro
+                    {
+                        // Llenamos el objeto cliente con los datos de la DB
+                        cliente.Id = (int)datos.Lector["Id"];
+                        cliente.Nombre = datos.Lector["Nombre"].ToString();
+                        cliente.Apellido = datos.Lector["Apellido"].ToString();
+                        cliente.Email = datos.Lector["Email"].ToString();
+                        cliente.Direccion = datos.Lector["Direccion"].ToString();
+                        cliente.Ciudad = datos.Lector["Ciudad"].ToString();
+                        cliente.Cp = (int)datos.Lector["Cp"];
+
+                        encontrado = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return encontrado;
+        }
+
+
     }
-}
+    }
